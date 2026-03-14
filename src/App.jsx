75bf28d270fce4d91f60191033b141c457ef9263
mrdmevans-rgb <mrdmevans@gmail.com>
2026-03-14 11:50:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -534,7 +534,7 @@ function Footer() {
             <div className="footer-contact-item"><MapPin size={16} className="icon-red" /> Amelia / Beechmont, Ohio</div>
             <div className="footer-contact-item"><Mail size={16} className="icon-red" /> info@hpcchurch.church</div>
 <div className="footer-contact-item">
-  <a href="https://facebook.com/https://www.facebook.com/profile.php?id=61583261639613" target="_blank" rel="noopener noreferrer">
+  <a href="https://www.facebook.com/profile.php?id=61583261639613" target="_blank" rel="noopener noreferrer">
     Follow us on Facebook
   </a>
 </div>
